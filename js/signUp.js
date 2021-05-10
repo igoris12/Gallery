37f162b70444253dc3451b01041ba894f34a8c64
igoris <igoris.x@inbox.lt>
2021-05-10 23:09:15 +0300

    add js

---
+++ js/signUp.js
@@ -0,0 +1,17 @@
+function myFunction() {
+    var x = document.getElementById("pass");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
+
+  function myFunctionR() {
+    var x = document.getElementById("passR");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
