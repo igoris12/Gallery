1a966334fb80d2d99c9dde29e982dcb82d1f89ac
igoris <igoris.x@inbox.lt>
2021-05-24 22:13:14 +0300

    random food position

--- js/components/snakegame/food.js
+++ js/components/snakegame/food.js
@@ -1,28 +1,36 @@
-import {expanSanek, onSnake} from './snake.js';
+import { expanSanek, onSnake } from './snake.js';
+import { randomGridPosition } from './grid.js';
 
-let food = { x: 1, y: 1}
+let food = getRandomFoodPosition()
 const EXPENSION = 1
 
- 
+
 function update() {
-   if (onSnake(food)) {
-       expanSanek(EXPENSION) 
-           food = {x:20, y:10}
-        
-   }
-   }
- 
- 
- function draw(gameBoard) {
-      const foodElement = document.createElement('div')
-      foodElement.style.gridRowStart = food.y
-      foodElement.style.gridColumnStart = food.x
-      foodElement.classList.add('food')
-      gameBoard.appendChild(foodElement)
-  
- }
+  if (onSnake(food)) {
+    expanSanek(EXPENSION)
+    food = getRandomFoodPosition()
+  }
+}
 
- 
 
- export {update, draw, }
- 
+function draw(gameBoard) {
+  const foodElement = document.createElement('div')
+  foodElement.style.gridRowStart = food.y
+  foodElement.style.gridColumnStart = food.x
+  foodElement.classList.add('food')
+  gameBoard.appendChild(foodElement)
+
+}
+
+function getRandomFoodPosition() {
+  let newFoodPosition
+  while (newFoodPosition == null || onSnake(newFoodPosition)) {
+    newFoodPosition = randomGridPosition()
+
+  }
+  return newFoodPosition
+}
+
+
+
+export { update, draw, }
